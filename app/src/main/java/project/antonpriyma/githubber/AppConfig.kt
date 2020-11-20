038192359e2db77ea93357963a3b6ac1e0ec02b0
a.priyma <a.priyma@corp.mail.ru>
2020-11-20 02:23:03 +0000

    [ADD]Init code

---
+++ app/src/main/java/project/antonpriyma/githubber/AppConfig.kt
@@ -0,0 +1,12 @@
+
+
+package com.antonpriyma.githubber
+
+object AppConfig {
+    const val GITHUB_API_BASE_URL = "https://api.github.com/"
+    const val OAUTH2_SCOPE = "user,repo,gist,notifications,admin"
+    const val SHARED_PREF = "com.antonpriyma.githubber.SHARED_PREF"
+    const val ACCESS_TOKEN = "com.antonpriyma.githubber.ACCESS_TOKEN"
+    const val LOGIN = "com.antonpriyma.githubber.LOGIN"
+    const val NAME = "com.antonpriyma.githubber.NAME"
+}
